16991a96cc7dc20ce64db31b2e354cce27bb5734
Brandon Thompson <thompson.brandon908@gmail.com>
2022-05-11 17:21:10 -0400

    Main functionality completed

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # AHEAD-Challenge
+1. Checks for hash length correctness with regular expresssion.
+2. Outputs the HTTP response code and gives detail for errors.
+3. Reports the number of flagged AV engines. 
 
 # TODO:
-1. Check for hash correctness
-2. Error messages for API key and hashes
-3. Output request status code
-4. Check and report number of AV engines ( 0, <=5, >5 )
 5. (Optional) generate hash from file
 6. (optional) get api-key from file
--- test-request.py
+++ test-request.py
@@ -1,45 +1,90 @@
-from nis import match
-from unicodedata import category
 import requests
 import sys
 import json
+import re
 
-SAFE_HASH = 'd14651e4b014d2d098c24ef76c7309da'
-BAD_HASH = '44d88612fea8a8f36de82e1278abb02f'
-TARGET = 'https://www.virustotal.com/api/v3/files/'
+SAFE_HASH_MD5 = 'd14651e4b014d2d098c24ef76c7309da'
+SAFE_HASH_SHA265 = '7539b2a70540ecd043b5a491aacd69060e5f1495fa28a6b3a09edc7888db6664'
+BAD_HASH_MD5 = '44d88612fea8a8f36de82e1278abb02f'
+BAD_HASH_SHA265 = '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'
+TARGET = 'https://www.virustotal.com/api/v3/files'
 HEADERS = {'x-apikey': ''}
 
+
+def check_hash(hash:str):
+    md5_check = re.findall(r"([a-fA-F\d]{32})", hash)
+    sha256_check = re.findall(r"([a-fA-F\d]{64})", hash)
+
+    if len(md5_check) == 1:
+        print("Hash is a valid MD5 hash.")
+    elif len(sha256_check) == 1:
+        print("Hash is a valid SHA256 hash.")
+    else:
+        print("Hash is not valid.")
+        exit(2)
 
 def run(hash, api_key):
     headers = {'x-apikey': api_key}
     url = f"{TARGET}/{hash}"
-    # response_safe = requests.get(TARGET + SAFE_HASH, headers=HEADERS) # safe response
-    # response_bad = requests.get(TARGET + BAD_HASH, headers=HEADERS) # bad response
-    response = requests.get(TARGET + BAD_HASH, headers=HEADERS)
-    print(f"Status Code: {response.status_code}")
-    
+    # response_safe = requests.get(TARGET + SAFE_HASH_MD5, headers=HEADERS) # safe response
+    # response_bad = requests.get(TARGET + BAD_HASH_MD5, headers=HEADERS) # bad response
+    response = requests.get(url, headers=headers)
 
-    if response.status_code == 401:
-         print("Status Code: 401\nUser account is not active or API key is not valid")
-         exit(1)
-    if response.status_code == 200:
-        print('Scan Success')
-        response_json = json.loads(response.text)
-        results = response_json['data']['attributes']['last_analysis_results']
+    if http_response_code(response.status_code):
+
+        response_json = json.loads(response.text)['data']
+        harmless_votes = response_json['attributes']['total_votes']['harmless']
+        malicious_votes = response_json['attributes']['total_votes']['malicious']
+        results = response_json['attributes']['last_analysis_results']
         count = 0
+
         for i in results:
             if results[i]['category'] == 'malicious':
                 count += 1
+        if count == 0:
+            print("File is clean!")
+        elif count < 5:
+            print(f"File may be malicious, {count} AV engines flagged the file")
+        elif count > 5:
+            print(f'File is malicious, flagged by {count} AV engines')
+        # print(f'{harmless_votes} people voted this file as harmless')
+        # print(f'{malicious_votes} people voted this file as malicious')
+    else:
+        print("There was an error processing the request.")
 
 
-    # match statements require python version > 3.10
-    # def http_response_code(status):
-    #     match status:
-    #         case 200:
+def http_response_code(status):
+    print(f'Status code: {status}')
+    if status == 200:
+        print('Scan successful!')
+        return True
+    elif status == 400:
+        print('Error: Bad request.')
+        return False
+    elif status == 401:
+        print('Error: Verify API key.')
+        return False
+    elif status == 403:
+        print('Error: You are not allowed to perform this operation.')
+        return False
+    elif status == 404:
+        print('Error: Requested resource was not available.')
+        return False
+    elif status == 429:
+        print("Error: Too many requests.")
+        return False
+    elif status == 503:
+        print('Error: Transient error, retry.')
+        return False
+    elif status == 504:
+        print('Error: Operation took too long.')
+        return False
+
 
 
 
 if __name__ == '__main__':
+    # Argparse not working correctly, fallback to sys.argv
     # parser = argparse.ArgumentParser(
     #     description="Virus Total API Tool",
     #     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -55,6 +100,7 @@ if __name__ == '__main__':
         print("Did not recieve correct number of arguments")
         print("Example: python file.py <hash_value> <api_key>")
     hash = sys.argv[1]
+    check_hash(hash)
     api_key = sys.argv[2]
 
     run(hash, api_key)
